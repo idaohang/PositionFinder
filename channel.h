73f4622489d7c1b91eee41214ebf744e046c9d1f
fangjin <jfang8@vt.edu>
2014-11-10 17:12:34 -0500

    add the readData member funtions.

--- channel.h
+++ channel.h
@@ -3,124 +3,23 @@
 #include <bitset>
 #include <vector>
 
-std::vector<int> prnGen(int sat_num)
+
+
+class channel
 {
-   std::vector<int> prn_code;
-   
-   std::bitset<10> g1_reg(0x3ff);
-   std::bitset<10> g2_reg(0x3ff);
-   for(int i = 0; i < 1023; ++i)
-   {
-      switch(sat_num)
-      {
-         case 1:
-            prn_code.push_back(g1_reg[9]^g2_reg[5]^g2_reg[1]);
-            break;
-         case 2:
-            prn_code.push_back(g1_reg[9]^g2_reg[6]^g2_reg[2]);
-            break;
-         case 3:
-            prn_code.push_back(g1_reg[9]^g2_reg[7]^g2_reg[3]);
-            break;
-         case 4:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[4]);
-            break;
-         case 5:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[0]);
-            break;
-         case 6:
-            prn_code.push_back(g1_reg[9]^g2_reg[9]^g2_reg[1]);
-            break;
-         case 7:
-            prn_code.push_back(g1_reg[9]^g2_reg[7]^g2_reg[0]);
-            break;
-         case 8:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[1]);
-            break;
-         case 9:
-            prn_code.push_back(g1_reg[9]^g2_reg[9]^g2_reg[2]);
-            break;
-         case 10:
-            prn_code.push_back(g1_reg[9]^g2_reg[2]^g2_reg[1]);
-            break;
-         case 11:
-            prn_code.push_back(g1_reg[9]^g2_reg[3]^g2_reg[2]);
-            break;
-         case 12:
-            prn_code.push_back(g1_reg[9]^g2_reg[5]^g2_reg[4]);
-            break;
-         case 13:
-            prn_code.push_back(g1_reg[9]^g2_reg[6]^g2_reg[5]);
-            break;
-         case 14:
-            prn_code.push_back(g1_reg[9]^g2_reg[7]^g2_reg[6]);
-            break;
-         case 15:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[7]);
-            break;
-         case 16:
-            prn_code.push_back(g1_reg[9]^g2_reg[9]^g2_reg[8]);
-            break;
-         case 17:
-            prn_code.push_back(g1_reg[9]^g2_reg[3]^g2_reg[0]);
-            break;
-         case 18:
-            prn_code.push_back(g1_reg[9]^g2_reg[4]^g2_reg[1]);
-            break;
-         case 19:
-            prn_code.push_back(g1_reg[9]^g2_reg[5]^g2_reg[2]);
-            break;
-         case 20:
-            prn_code.push_back(g1_reg[9]^g2_reg[6]^g2_reg[3]);
-            break;
-         case 21:
-            prn_code.push_back(g1_reg[9]^g2_reg[7]^g2_reg[4]);
-            break;
-         case 22:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[5]);
-            break;
-         case 23:
-            prn_code.push_back(g1_reg[9]^g2_reg[2]^g2_reg[0]);
-            break;
-         case 24:
-            prn_code.push_back(g1_reg[9]^g2_reg[5]^g2_reg[3]);
-            break;
-         case 25:
-            prn_code.push_back(g1_reg[9]^g2_reg[6]^g2_reg[4]);
-            break;
-         case 26:
-            prn_code.push_back(g1_reg[9]^g2_reg[7]^g2_reg[5]);
-            break;
-         case 27:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[6]);
-            break;
-         case 28:
-            prn_code.push_back(g1_reg[9]^g2_reg[9]^g2_reg[7]);
-            break;
-         case 29:
-            prn_code.push_back(g1_reg[9]^g2_reg[5]^g2_reg[0]);
-            break;
-         case 30:
-            prn_code.push_back(g1_reg[9]^g2_reg[6]^g2_reg[1]);
-            break;
-         case 31:
-            prn_code.push_back(g1_reg[9]^g2_reg[7]^g2_reg[2]);
-            break;
-         case 32:
-            prn_code.push_back(g1_reg[9]^g2_reg[8]^g2_reg[3]);
-            break;
-         default:
-            break;
-      }
-
-      int tmp1 = g1_reg[9]^g1_reg[2];
-      int tmp2 = g2_reg[9]^g2_reg[8]^g2_reg[7]^g2_reg[5]^g2_reg[2]^g2_reg[1];
-      g1_reg = g1_reg << 1;
-      g2_reg = g2_reg << 1;
-      g1_reg[0] = tmp1;
-      g2_reg[0] = tmp2;
-   }
-   return prn_code;
-}
-
+   public:
+      enum Status{IDLE, ACQUISITION, PULL, TRACKING};
+      channel(int chann_num):chann_id{chann_num},state{IDLE}{ prnGen(chann_num); }
+      std::vector<int> prnSequence() { return prn; } 
+      static void readData(std::istream&, size_t);
+      static void dataShown();
+   private:
+      static const double fs;
+      static const double fo;
+      int chann_id;
+      Status state;
+      static std::vector<int> raw_data;
+      std::vector<int> prn;
+      void prnGen(int sat_num);
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -1,144 +1,51 @@
 #include <iostream>
 #include <bitset>
 #include <vector>
+#include <fstream>
+#include "channel.h"
 
 
 using namespace std;
 
-
-vector<int> prnGen(int sat_num)
-{
-   vector<int> prn_code;
-   
-   bitset<10> g1_gen(0x3ff);
-   bitset<10> g2_gen(0x3ff);
-   for(int i=0; i<1023; ++i)
-   {
-      switch(sat_num)
-      {
-         case 1:
-            prn_code.push_back(g1_gen[9]^g2_gen[5]^g2_gen[1]);
-            break;
-         case 2:
-            prn_code.push_back(g1_gen[9]^g2_gen[6]^g2_gen[2]);
-            break;
-         case 3:
-            prn_code.push_back(g1_gen[9]^g2_gen[7]^g2_gen[3]);
-            break;
-         case 4:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[4]);
-            break;
-         case 5:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[0]);
-            break;
-         case 6:
-            prn_code.push_back(g1_gen[9]^g2_gen[9]^g2_gen[1]);
-            break;
-         case 7:
-            prn_code.push_back(g1_gen[9]^g2_gen[7]^g2_gen[0]);
-            break;
-         case 8:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[1]);
-            break;
-         case 9:
-            prn_code.push_back(g1_gen[9]^g2_gen[9]^g2_gen[2]);
-            break;
-         case 10:
-            prn_code.push_back(g1_gen[9]^g2_gen[2]^g2_gen[1]);
-            break;
-         case 11:
-            prn_code.push_back(g1_gen[9]^g2_gen[3]^g2_gen[2]);
-            break;
-         case 12:
-            prn_code.push_back(g1_gen[9]^g2_gen[5]^g2_gen[4]);
-            break;
-         case 13:
-            prn_code.push_back(g1_gen[9]^g2_gen[6]^g2_gen[5]);
-            break;
-         case 14:
-            prn_code.push_back(g1_gen[9]^g2_gen[7]^g2_gen[6]);
-            break;
-         case 15:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[7]);
-            break;
-         case 16:
-            prn_code.push_back(g1_gen[9]^g2_gen[9]^g2_gen[8]);
-            break;
-         case 17:
-            prn_code.push_back(g1_gen[9]^g2_gen[3]^g2_gen[0]);
-            break;
-         case 18:
-            prn_code.push_back(g1_gen[9]^g2_gen[4]^g2_gen[1]);
-            break;
-         case 19:
-            prn_code.push_back(g1_gen[9]^g2_gen[5]^g2_gen[2]);
-            break;
-         case 20:
-            prn_code.push_back(g1_gen[9]^g2_gen[6]^g2_gen[3]);
-            break;
-         case 21:
-            prn_code.push_back(g1_gen[9]^g2_gen[7]^g2_gen[4]);
-            break;
-         case 22:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[5]);
-            break;
-         case 23:
-            prn_code.push_back(g1_gen[9]^g2_gen[2]^g2_gen[0]);
-            break;
-         case 24:
-            prn_code.push_back(g1_gen[9]^g2_gen[5]^g2_gen[3]);
-            break;
-         case 25:
-            prn_code.push_back(g1_gen[9]^g2_gen[6]^g2_gen[4]);
-            break;
-         case 26:
-            prn_code.push_back(g1_gen[9]^g2_gen[7]^g2_gen[5]);
-            break;
-         case 27:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[6]);
-            break;
-         case 28:
-            prn_code.push_back(g1_gen[9]^g2_gen[9]^g2_gen[7]);
-            break;
-         case 29:
-            prn_code.push_back(g1_gen[9]^g2_gen[5]^g2_gen[0]);
-            break;
-         case 30:
-            prn_code.push_back(g1_gen[9]^g2_gen[6]^g2_gen[1]);
-            break;
-         case 31:
-            prn_code.push_back(g1_gen[9]^g2_gen[7]^g2_gen[2]);
-            break;
-         case 32:
-            prn_code.push_back(g1_gen[9]^g2_gen[8]^g2_gen[3]);
-            break;
-         default:
-            break;
-      }
-
-      int tmp1 = g1_gen[9]^g1_gen[2];
-      int tmp2 = g2_gen[9]^g2_gen[8]^g2_gen[7]^g2_gen[5]^g2_gen[2]^g2_gen[1];
-      g1_gen = g1_gen << 1;
-      g2_gen = g2_gen << 1;
-      g1_gen[0] = tmp1;
-      g2_gen[0] = tmp2;
-   }
-   return prn_code;
-}
-
-
 int main()
 {
+   ifstream inFile("GPS_fs16_3676E6-if4_1304E6_8bit_Real.bin", ios::in | ios::binary);
 
-   for(int i=1; i<33; ++i)
+   if(!inFile)
    {
-      cout << i << ": ";
-   vector<int> prn = prnGen(i);
-   for(auto i = 0; i<10; ++i)
+      cerr << "the input file does not exist.\n";
+      return -1;
+   }
+   
+   char buffer[1000];
+
+   inFile.read(buffer,1000);
+
+   for(int i = 0; i < 1000; ++i)
    {
-      cout << prn[i];
+      cout <<int(buffer[i]) << " ";
    }
-   cout << endl;
+
+   /*
+   vector<channel> ch;
+   for(int i=0; i<32; ++i)
+   {
+      channel cur_ch(i+1);
+      ch.push_back(cur_ch);
    }
+   for(auto i = 0; i<ch.size(); ++i)
+   {
+      vector<int> prnSeq = ch[i].prnSequence();
+      cout << i+1 << ": ";
+      for(auto it = prnSeq.begin(); it!=prnSeq.end(); ++it)
+      {
+         cout << *it;
+      }
+      cout<<endl<<endl;
+   }
+
+   channel::readData(inFile, 10);
+   channel::dataShown();
+   */
    return 0;
 }
